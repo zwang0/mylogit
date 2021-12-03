0bfada51600ceb92c193a9301d4e552e8b9e7d65
Zehua Wang <wangzeh@umich.edu>
2021-12-02 19:33:25 -0500

    Write mylogitEst function

--- R/mylogit.R
+++ R/mylogit.R
@@ -2,6 +2,8 @@
 #'
 #'mylogit is used to fit a logistic regression model.
 #'
+#'@import stats
+#'
 #'@param formula a symbolic description of the model to be fitted.
 #'@param data a data frame containing the variables in the model.
 #'
@@ -12,18 +14,34 @@
 #'
 #'@export
 #'
-mylogit = function(formula, data) {
+mylogitEst = function(X, y, criteria, break_loop) {
+  y_num = as.matrix(ifelse(y == level[1], 0, 1)) # set y into 0/1
+  beta = matrix(0, p+1, 1) # initialize beta
+  eps = 1 # initial criteria
+  run_loop = 0 # initial running loop
+  # loop to converge beta
+  while (eps >= criteria || run_loop < break_loop) {
+    prob = 1/(1+exp(-X %*% beta)) # X is n by p+1, beta is p+1 by 1
+    X_tilde = as.numeric(prob*(1-prob))*X
+    beta_new = beta + solve(t(X) %*% X_tilde) %*% (t(X) %*% (y_num - prob))
+    eps = max(abs(beta_new - beta)) # compute criteria
+    beta = beta_new # update beta
+    run_loop =  run_loop + 1 # update run_loop
+  }
+  return(beta)
+}
+
+mylogit.default = function(formula, data, criteria = 0.001, break_loop = 10000) {
   # extra x and y
   mf = model.frame(formula=formula, data=data)
   X = model.matrix(attr(mf, "terms"), data=mf)
   y = model.response(mf)
-  n = length(y)
+  n = dim(X)[1]
+  p = dim(X)[2] - 1
   level = levels(y)
-  y_num = ifelse(y == level[1], 1, 0) # set y into 1/0
-  beta = matrix(0, n, 2) # initialize beta
+  est = mylogitEst(X, y, criteria, break_loop)
 
-  while (eps < 0.01)
-  p = exp(beta %*% x)
+  est$fitted.values = 1/(1+exp(-X %*% coef))
+  est$residuals = y - est$fitted.values
 
-  return(y)
 }
